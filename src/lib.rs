225979aff9ac74b2268c036dd13119e12d701f57
WITstudio <1656800105@qq.com>
2024-07-09 21:33:50 +0800

    实现密码生成

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod cli_resolve;
-mod process_csv;
+mod process;
 mod toml_tostring;
 
 pub use cli_resolve::{Args, Command, Csvcmd, FormatType};
-pub use process_csv::{process_csv, show, verify_file_exist};
+pub use process::{generate_password, process_csv, show, verify_file_exist};
 pub use toml_tostring::to_toml_string;
