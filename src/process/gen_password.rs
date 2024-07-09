e04bf57a41cc152507a8286669ee5467648b13e4
WITstudio <1656800105@qq.com>
2024-07-09 21:42:23 +0800

    add password verify

---
+++ src/process/gen_password.rs
@@ -0,0 +1,77 @@
+use std::process;
+
+use anyhow::Result;
+use colored::Colorize;
+use rand::seq::SliceRandom;
+
+const UPPERCHARS: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
+const LOWERCHARS: &[u8] = b"abcdefghijkmnopqrstuvwxyz";
+const SYMBOLS: &[u8] = b"!@#$%^&*_";
+const NUMBERS: &[u8] = b"123456789";
+
+pub fn generate_password(
+    length: u8,
+    no_upper: bool,
+    no_lower: bool,
+    no_symbol: bool,
+    no_number: bool,
+) -> String {
+    // 验证密码
+    if let Err(e) = verify_password(no_upper, no_lower, no_symbol, no_number) {
+        // 在什么都包含的情况下输出错误及提示信息
+        eprintln!("{e}");
+        // 终止程序
+        process::exit(1);
+    }
+
+    // 创建一个随机数生成器
+    let mut rng = rand::thread_rng();
+    // 备选区域
+    let mut chars: Vec<u8> = Vec::new();
+    // 已选区域
+    let mut password: Vec<u8> = Vec::new();
+    // TODO: Implement password generation
+    if !no_upper {
+        chars.extend(UPPERCHARS);
+        // 确保结果中至少有一个大写字母
+        password.push(*UPPERCHARS.choose(&mut rng).unwrap());
+    }
+    if !no_lower {
+        chars.extend(LOWERCHARS);
+        password.push(*LOWERCHARS.choose(&mut rng).unwrap());
+    }
+    if !no_symbol {
+        chars.extend(SYMBOLS);
+        password.push(*SYMBOLS.choose(&mut rng).unwrap());
+    }
+    if !no_number {
+        chars.extend(NUMBERS);
+        password.push(*NUMBERS.choose(&mut rng).unwrap());
+    }
+
+    // 生成剩余的字符
+    for _ in 0..(length - password.len() as u8) {
+        password.push(*chars.choose(&mut rng).unwrap());
+    }
+
+    // 乱序处理
+    password.shuffle(&mut rng);
+
+    String::from_utf8(password).unwrap()
+}
+
+fn verify_password(
+    no_upper: bool,
+    no_lower: bool,
+    no_symbol: bool,
+    no_number: bool,
+) -> Result<(), String> {
+    if no_upper && no_lower && no_symbol && no_number {
+        return Err(format!(
+            "{} No character type selected , {}",
+            "Error:".red(),
+            "please select at least one character type".yellow()
+        ));
+    }
+    Ok(())
+}
---
+++ src/process/process_csv.rs
@@ -0,0 +1,92 @@
+use anyhow::Result;
+use colored::Colorize;
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+use std::{fs, path::Path, process};
+
+use crate::{to_toml_string, FormatType};
+// 解析 csv 文件的结构体
+#[derive(Debug, Deserialize, Serialize)]
+// 定义解析的目标名 , 所有设置为首字母大写
+#[serde(rename_all = "PascalCase")]
+pub struct Record {
+    name: String,
+    position: String,
+    #[serde(rename = "DOB")] // 个别需要单独设置
+    dob: String,
+    nationality: String,
+    #[serde(rename = "Kit Number")]
+    kit_number: u8,
+}
+
+// 验证文件是否存在
+pub fn verify_file_exist(filename: &str, replace: bool) {
+    // 如果在非替换模式下重名 , painc 程序
+    if !replace && Path::new(filename).exists() {
+        println!(
+            "{} invalid value {} for '--output <OUTPUT>': {} \n\n For more information, try '-r'.",
+            "error:".red(),
+            format!("'{}'", filename).yellow(),
+            "file already exists".blue()
+        );
+        // 不继续写入 , 结束程序
+        process::exit(1);
+    }
+}
+
+// 将 csv 转化为 对应文件
+pub fn process_csv(input: &str, output: &str, format_type: FormatType) -> Result<()> {
+    let mut _ret = String::new();
+
+    // 读取 csv 文件
+    let mut reader = Reader::from_path(input)?;
+    // 遍历 reader 中的所有 record
+    let header = reader.headers()?.clone();
+
+    // 判断什么生成类型
+    match format_type {
+        FormatType::Toml => {
+            _ret = to_toml_string(header, &mut reader)?;
+        }
+        _ => {
+            let mut ret_vec = Vec::with_capacity(128);
+            for record in reader.records() {
+                let record = record?;
+                let zipped = header
+                    .iter()
+                    .zip(record.iter())
+                    .collect::<serde_json::Value>();
+                ret_vec.push(zipped);
+            }
+            _ret = serde_json::to_string(&ret_vec)?;
+        }
+    }
+
+    // 写入目标文件
+    fs::write(output, _ret)?;
+    println!("写入完成");
+    Ok(())
+}
+
+// 打印 csv::reader 的 record
+macro_rules! print_record {
+    ($record:expr,$color:ident) => {
+        print!("{} ", "|".yellow());
+        for i in $record.iter() {
+            // 40个字符
+            print!("{:30}{}  ", i.$color(), "|".yellow());
+        }
+        println!("\n{}", "-".to_string().repeat(165).yellow());
+    };
+}
+
+// 将 csv 打印到终端
+pub fn show(filename: &str) -> Result<()> {
+    let mut reader = Reader::from_path(filename)?;
+    let header = reader.headers()?.clone();
+    print_record!(header, blue);
+    for record in reader.records() {
+        print_record!(record?, green);
+    }
+    Ok(())
+}
